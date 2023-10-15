2c6d17aea2de62a8d9301018693e6dd72de25ade
Yan Luiz <yan.luiz.lima@gmail.com>
2023-10-15 20:10:46 -0300

    inserindo a funcionalidade de cadastrar usuarios

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,18 +1,24 @@
 const AppError = require("../utils/App.error")
 
+const sqliteConnection = require('../database/sqlite')
 
 class UserController {
 
 
 
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body
 
-        if(!name) {
-            throw new AppError("o nome é obrigatorio");
-        }    
+        const database = await sqliteConnection();
+        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-        response.status(201).json(  { name })
+        if(checkUserExist) {
+            throw new AppError("Este e-mail já está em uso.")
+        }
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password])
+
+        return response.status(201).json()
     } 
 }
 
